8e441105570fb7ecc86b6089bc3c488cf6a40f75
79260 <792602334>
2020-12-22 20:48:21 +0800

    1

---
+++ C89-S2-pet/src/main/java/yc/rng/pet/PetApplication.java
@@ -0,0 +1,14 @@
+package yc.rng.pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class PetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetApplication.class, args);
+	}
+}
